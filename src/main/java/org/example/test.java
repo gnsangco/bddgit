da44a22d7f44f5dde9986a9695438addd76dc9df
gnsangco <gnsangco@gmail.com>
2023-06-01 17:24:12 +0800

    bddgit-2

--- src/main/java/org/example/test.java
+++ src/main/java/org/example/test.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public class test {
+
+    public void a1(){
+        System.out.println("a1 notes");
+    }
+    public void a2(){
+        System.out.println("a2 notes");
+    }
 }
